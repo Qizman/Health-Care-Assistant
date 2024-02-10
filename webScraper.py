1703ab7abc95f6dc2729c146b79fa9604d50225b
Qizman <michael.simon@live.com>
2024-02-10 12:41:22 -0500

    Web Scraper for Sample Diseases

---
+++ webScraper.py
@@ -0,0 +1,37 @@
+#import library
+import json
+#!pip install selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+def update_Diseases():
+    #get json list
+    with open('diseases.json') as f:
+        try:
+            data = json.load(f)
+        except ValueError:
+            print("File is empty")
+            data = []
+            exit()
+    #for loop to go through the list
+    for i in data:
+        try:
+            print(i)
+            #open browser
+            browser = webdriver.Firefox()
+            #go to the website
+            browser.get(i["url"])
+            #print main content from element class
+            i["info"] = browser.find_element(By.CLASS_NAME, "inner-article-container").text
+            #save json file
+            with open('diseases.json', 'w') as f:
+                json.dump(data, f)
+            #quit the browser
+            browser.quit()
+        except Exception as e:
+            print("Error: ", e)
+            browser.quit()
+            continue
+update_Diseases()
+
